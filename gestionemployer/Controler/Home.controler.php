f759320586b065a02644cf7b8fbef11bf029d5bd
Sapae Safae <sapaebaamel@gmail.com>
2021-05-02 01:35:58 +0000

    new small project

--- gestionemployer/Controler/Home.controler.php
+++ gestionemployer/Controler/Home.controler.php
@@ -1,10 +1,12 @@
-<?php
+<?php 
 
-    Class HomeControler {
 
-        public function index($page) {
-            include ('Views/' .$page.'.php');
-        }
-    }
+class HomeController{
+
+	public function index($page){
+		include('Views/'.$page.'.php');
+	}
+
+}
 
 ?>
--- gestionemployer/index.php
+++ gestionemployer/index.php
@@ -1,21 +1,31 @@
 <?php
 
     require_once './autoload.php';
+    require_once './Views/Includes/header.php';
     require_once './Controler/Home.controler.php';
+    $home = new HomeController();
 
-    $home = new HomeControler;
-
-    $pages = ['home', 'update', 'delete', 'add'];
-
-    if(isset($_GET['page'])) {
-        if (in_array($_GET['page'], $pages)) {
-            $page = $_GET['page'];
-            $home->index($page);
-        } else {
-            include ('Views/Includes/404.php');
+    $pages = ['home','add','update','delete','logout'];
+    
+    if(isset($_SESSION['logged']) && $_SESSION['logged'] === true){
+    
+        if(isset($_GET['page'])){
+            if(in_array($_GET['page'],$pages)){
+                $page = $_GET['page'];
+                $home->index($page);
+            }else{
+                include('views/includes/404.php');
+            }
+        }else{
+            $home->index('home');
         }
-    } else {
-        $home->index('home');
+    
+    
+    require_once './views/includes/footer.php';
+    
+    
+    }else if(isset($_GET['page']) && $_GET['page'] === 'register'){
+        $home->index('register');
+    }else{
+        $home->index('login');
     }
-
-?>
